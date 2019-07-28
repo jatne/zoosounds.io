c5326d4eed3298a83d989baea71189eee87f6323
Paweł Malok <jatne@MacBook-Air-Pawe.local>
2019-07-28 21:49:21 +0200

    component: BtnInvertColors

---
+++ src/components/BtnInvertColors.vue
@@ -0,0 +1,14 @@
+<template>
+  <button v-on:click="invertColors">Invert colors</button>
+</template>
+
+<script>
+export default {
+  name: "BtnInvertColors",
+  methods: {
+    invertColors() {
+      this.$parent.isInvertedColors = !this.$parent.isInvertedColors;
+    }
+  }
+};
+</script>
