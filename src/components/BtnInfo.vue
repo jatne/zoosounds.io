bdeadbc974b8576ab6d65f18ede80218be323ab4
Paweł Malok <jatne@MacBook-Air-Pawe.local>
2019-07-28 21:49:10 +0200

    component: BtnInfo

---
+++ src/components/BtnInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <button type="button" v-on:click="toggleInfo">
+    <span v-if="isActive">made with</span> ❤️
+    <span v-if="isActive">for Hania</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BtnInfo",
+  data() {
+    return {
+      isActive: false
+    };
+  },
+  methods: {
+    toggleInfo() {
+      this.isActive = !this.isActive;
+    }
+  }
+};
+</script>
