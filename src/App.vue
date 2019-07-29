b3d05cf04f39884559f20ea1994e09ec7c5781ea
Paweł Malok <jatne@MacBook-Air-Pawe.local>
2019-07-29 07:18:00 +0200

    reorganize by vue-routing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
   <div id="app" v-bind:class="isInvertedColors ? 'is-inverted' : ''">
-    <main>
-      <Slider></Slider>
-    </main>
-    <footer>
-      <BtnInvertColors></BtnInvertColors>
-      <BtnInfo></BtnInfo>
-    </footer>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Slider from "@/components/Slider";
-import BtnInvertColors from "@/components/BtnInvertColors";
-import BtnInfo from "@/components/BtnInfo";
-
 export default {
   data() {
     return { isInvertedColors: false };
-  },
-  components: {
-    Slider,
-    BtnInvertColors,
-    BtnInfo
   }
 };
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <main>
+      <Slider></Slider>
+    </main>
+    <footer>
+      <BtnInvertColors></BtnInvertColors>
+      <BtnInfo></BtnInfo>
+    </footer>
+    </div>
+</template>
+
+<script>
+import Slider from "@/components/Slider";
+import BtnInvertColors from "@/components/BtnInvertColors";
+import BtnInfo from "@/components/BtnInfo";
+
+export default {
+  name: 'home',
+  components: {
+    Slider,BtnInvertColors,BtnInfo
+  }
+}
+</script>
